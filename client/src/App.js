d5f5822671717d2202faa5c37fff1a45a7e13712
Bruno Barbero <colo_barbero16@hotmail.com>
2023-05-15 16:09:07 -0300

    segundo commit PI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './views/HomePage/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <Home></Home>
     </div>
   );
 }
